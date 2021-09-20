8b13ea804f21172f9c179865df832415d3ee4f49
Shreyansh Jain <shreyjay0@gmail.com>
2021-09-20 18:00:45 +0530

    add

---
+++ colors.py
@@ -0,0 +1,13 @@
+#colors
+blue = "\033[1;34m"
+cyan="\033[0;36m"
+black="\033[0;30m"
+white="\033[0;37m"
+green="\033[0;32m"
+bred="\033[1;31m"
+red="\033[0;91m"
+yellow="\033[0;33m"
+back_yellow = "\033[43m"
+
+def styless(blue, bred, red, green, yellow):
+    return blue, bred, red, green, yellow
