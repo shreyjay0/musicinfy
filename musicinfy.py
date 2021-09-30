9f979963995c94fef64484b11df7afae759c97c7
Shreyansh Jain <shreyjay0@gmail.com>
2021-09-30 09:01:45 +0530

    feat:authentication improvement #1

--- musicinfy.py
+++ musicinfy.py
@@ -1,15 +1,11 @@
-from requests import request
-import os
-import time
-import sys
-import requests
-import json
+import os, time,sys
+from typing import OrderedDict
+import requests,json
 import pandas as pd
-import styless
-# headers
-headers = {
-    'Authorization': 'Bearer Token_HERE'  }
+from t import *
+from styless import *
 
+col = Colors()
 #globally used 
 opt = ["n","no"]
 user_data = {}
@@ -36,19 +32,30 @@ def prints(txt):
 #type out input text
 def inputs(txt):
   prints(txt)
-  print(styless.red + user_data["display_name"]+ " > ", end="")
+  print(col.red + user_data["display_name"]+ " > ", end="")
   return input()
 
 #clears screen 
 def clear_screen(): 
   os.system('cls' if os.name == 'nt' else 'clear')
 
+#sends redirect notice
+def redirect_notice():
+  prints(col.green+musicinfy_full)
+  prints("Welcome to Musicinfy!"+col.u_end)
+  prints(col.purple+"""
+  To get started we need you to allow us to make changes to your spotify 
+  account (includes playlist creation, updation and deletion).
+  And dont't worry this data isn't stored anywhere. 
+
+  You will now be redirected to spotify authentication page.""")
+
 #get profile 
 def get_profile():
   profile_url = "https://api.spotify.com/v1/me"
   res = requests.request("GET", profile_url, headers=headers, data={})
   if res.status_code not in list(range(200,206)):
-    prints(styless.red+"Error "+str(res.status_code)+ ": "+res.reason)
+    prints(col.red+"Error "+str(res.status_code)+ ": "+res.reason)
     exit()
   else:
     user_data = res.json()
@@ -57,33 +64,33 @@ def get_profile():
 #get playlist info from user
 def playlist_details():
   public = True
-  prints(musicinfy_full)
-  prints(styless.green + styless.bold + "Hey " + user_data["display_name"] +"! Welcome to Musicinfy. We need some details for the playlist that you wanna create."+ styless.u_end)
-  prints(styless.yellow+ styless.u_yellow +"Note that anything other than (yes/no), (y/n) [not case sesnsitive] would be considered a yes."+styless.u_end)
+  prints(col.green + col.bold + "Thank you " + user_data["display_name"] +"! Now we need some details for the playlist that you wanna create."+ col.u_end)
+  prints(col.yellow+ col.u_yellow +"Note that anything other than (yes/no), (y/n) [not case sesnsitive] would be considered a yes."+col.u_end)
+  time.sleep(1)
   name_confirm = False
   while (not name_confirm):
     playlist_name = ""
     while playlist_name=="":
-      playlist_name = inputs(styless.blue+"Please enter the name you wanna give to the playlist: ")
+      playlist_name = inputs(col.blue+"Please enter the name you wanna give to the playlist: ")
       clear_screen()
-    req_desc = inputs(styless.blue+"Do you want to give the playlist a description? (Y/N) ")
+    req_desc = inputs(col.blue+"Do you want to give the playlist a description? (Y/N) ")
     clear_screen()
     if req_desc.lower() not in opt:
-      desc = inputs(styless.blue+"Please enter the description: ")
+      desc = inputs(col.blue+"Please enter the description: ")
       clear_screen()
     else: desc =""
-    if inputs(styless.blue+"Do you want to make the playlist private? (Y/N) ").lower() not in opt:
+    if inputs(col.blue+"Do you want to make the playlist private? (Y/N) ").lower() not in opt:
       public = False
     clear_screen()
     prints("Please confirm the details below:")
     prints("Playlist name: "+ playlist_name)
-    if not req_desc:
+    if req_desc.lower() not in opt:
       prints("Playlist description: "+ desc)
     else: prints("No description provided.")
     if public:
       prints("The playlist will be public.")
     else: prints("The playlist will be private.")
-    inp = inputs(styless.blue+"Do you confirm the details? (Y/N) ")
+    inp = inputs(col.blue+"Do you confirm the details? (Y/N) ")
     clear_screen()
     if inp.lower() not in opt:
       name_confirm =  True 
@@ -104,7 +111,7 @@ def create_playlist():
   })
   res = requests.request("POST", create_playlist_url, headers=headers, data=data)
   if res.status_code not in list(range(200,206)):
-    print (styless.red+"Error "+str(res.status_code)+ ": "+res.reason)
+    print (col.red+"Error "+str(res.status_code)+ ": "+res.reason)
     exit()
   else:
     time.sleep(0.05)
@@ -116,7 +123,7 @@ def create_playlist():
 #fetches file name from the given location
 def fetch_file_names():
   fpath = None #default
-  path_input = inputs(styless.blue+"Enter the absolute location of the folder with all the song files (press enter if it's in the root directory): ")
+  path_input = inputs(col.blue+"Enter the absolute location of the folder with all the song files (press enter if it's in the root directory): ")
   if path_input:
     fpath = path_input
   # files = next(os.walk(fpath))[2]
@@ -164,7 +171,7 @@ def fetch_song_link(name,formatted_name):
       links.append(link)
     return link
   else:
-    prints(styless.red+"Error "+str(res.status_code)+ ": "+res.reason)
+    prints(col.red+"Error "+str(res.status_code)+ ": "+res.reason)
     exit()
 
 #exports songs 
@@ -187,7 +194,7 @@ def add_by_link(link):
   url = "https://api.spotify.com/v1/playlists/"+created_playlist_id+"/tracks?uris="+get_uri(link)
   res = requests.request("POST", url, headers=headers, data={})
   if res.status_code not in list(range(200,206)):
-    prints(styless.red+"Error "+str(res.status_code)+ ": "+res.reason+ ". Track wasn't added to playlist.")
+    prints(col.red+"Error "+str(res.status_code)+ ": "+res.reason+ ". Track wasn't added to playlist.")
     return False
   else:
     return True
@@ -227,6 +234,17 @@ def add_to_playlist():
   prints("You can now enjoy your old tracks and get nostalgic. \nThank you for using me.")
 
 clear_screen()
+redirect_notice()
+time.sleep(5)
+auth_token, refresh_token = Auth().user_authentication()
+# headers
+headers = {
+    "Authorization": f"Bearer {auth_token}"
+  }
+
+clear_screen()
+print(auth_token)
+time.sleep(5)
 user_data = get_profile()
 id = user_data["id"]
 playlist_name , desc ,public = playlist_details()
